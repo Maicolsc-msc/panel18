6df7e58c3be535425b7be4999454d7ea92479eda
Maicolsc-msc <64378356+Maicolsc-msc@users.noreply.github.com>
2023-06-22 09:52:52 -0500

    Add files via upload

--- ubuinst.sh
+++ ubuinst.sh
@@ -259,13 +259,13 @@ clear
 }
 function phpmadm {
 cd /usr/share || exit
-wget https://files.phpmyadmin.net/phpMyAdmin/5.2.0/phpMyAdmin-5.2.0-all-languages.zip > /dev/null 2>&1
-unzip phpMyAdmin-5.2.0-all-languages.zip > /dev/null 2>&1
-mv phpMyAdmin-5.2.0-all-languages phpmyadmin > /dev/null 2>&1
+wget https://files.phpmyadmin.net/phpMyAdmin/5.2.1/phpMyAdmin-5.2.1-all-languages.zip > /dev/null 2>&1
+unzip phpMyAdmin-5.2.1-all-languages.zip > /dev/null 2>&1
+mv phpMyAdmin-5.2.1-all-languages phpmyadmin > /dev/null 2>&1
 chmod -R 0777 phpmyadmin > /dev/null 2>&1
 ln -s /usr/share/phpmyadmin /var/www/html/phpmyadmin > /dev/null 2>&1
 systemctl restart apache2 > /dev/null 2>&1 
-rm phpMyAdmin-5.2.0-all-languages.zip > /dev/null 2>&1
+rm phpMyAdmin-5.2.1-all-languages.zip > /dev/null 2>&1
 cd /root || exit
 }
 
